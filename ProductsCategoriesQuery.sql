23dac81f10b1863c2ca5dff0035a9a2a76a97205
ilyakurilev <60757191+ilyakurilev@users.noreply.github.com>
2021-02-08 18:55:30 +0300

    Update ProductsCategoriesQuery.sql

--- ProductsCategoriesQuery.sql
+++ ProductsCategoriesQuery.sql
@@ -1,7 +1,7 @@
 SELECT P.[Name] AS 'Product',
-	   C.[Name] AS 'Category'
-FROM [Products] AS P
-LEFT JOIN [ProductsCategories] AS PC
-  ON P.Id = PC.ProductId
-LEFT JOIN [Categories] AS C
-  ON C.Id = PC.CategoryId;
+       C.[Name] AS 'Category'
+  FROM [Products] AS P
+  LEFT JOIN [ProductsCategories] AS PC
+    ON P.Id = PC.ProductId
+  LEFT JOIN [Categories] AS C
+    ON C.Id = PC.CategoryId;
